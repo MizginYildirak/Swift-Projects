a63f6fd2ba06804b55a4fe4e4748cf281252b5f0
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2023-12-31 00:35:52 +0300

    add progressBar and sound

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,35 +7,61 @@
 //
 
 import UIKit
-
+import AVFoundation
 
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
+        var player: AVAudioPlayer?
+        var timer = Timer()
         var counter = 0
-    
+        var secondsPassed = 0
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
 
+        timer.invalidate()
         let hardness = sender.currentTitle!
         counter = eggTimes[hardness]!
        startTimer()
-      
+
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
     }
     
    func startTimer() {
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func playSound() {
+     let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        
+        player?.play()
     }
     
     
     @objc func updateCounter() {
-        if counter > 0 {
+        if secondsPassed < counter {
+            secondsPassed += 1
+            _ = secondsPassed / counter
+            
+            progressBar.progress = Float(secondsPassed) / Float(counter)
             print("\(counter) seconds")
-            counter -= 1
+        
+        } else {
+            timer.invalidate()
+            playSound()
+            titleLabel.text = "DONE!"
         }
     }
-    
 }
