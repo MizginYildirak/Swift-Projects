97fe4c5ab324787e1de1816321a1158f8793a191
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-03 18:12:30 +0300

    add progress bar and feedback

---
+++ Quizzler-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by MİZGİN YILDIRAK on 2.01.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
