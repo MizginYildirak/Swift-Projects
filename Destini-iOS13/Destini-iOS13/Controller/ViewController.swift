851d878b1924686dc3864f972a1aefd13a912d1e
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-09 03:11:54 +0300

    initial commit

---
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        storyLabel.text = storyBrain.getStoryTitle()
+        
+//        choice1Button.setTitle(choice1, for: .normal)
+//        choice2Button.setTitle(choice2, for: .normal)
+    }
+
+
+}
+
