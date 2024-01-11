6d04246fdbb2a1c0ecf89779f6e0930992197658
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-11 03:05:28 +0300

    initial commit

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+    }
+}
+
