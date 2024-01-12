f6909f78088b1a88ef0f8e8d164ba28fa0472e87
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-12 03:25:45 +0300

    add SecondViewController

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,16 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = height / (height * height)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        print(bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
