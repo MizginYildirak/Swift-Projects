bad648ae2ba197b88759c480513bb4a55f9e007b
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-13 17:23:03 +0300

    add segues and navigation

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by MİZGİN YILDIRAK on 13.01.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by MİZGİN YILDIRAK on 12.01.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
