d3612b9c6e9d883faf06596e7e800ada18ab7217
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-01-25 01:19:18 +0300

    add delegates and extensions

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
-
+class WeatherViewController: UIViewController {
+    func didUpdateteWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        <#code#>
+    }
+    
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
@@ -17,43 +21,65 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     
     var weatherManager = WeatherManager()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        weatherManager.delegate = self
         searchTextField.delegate = self
     }
-    
-    
-    @IBAction func searchPressed(_ sender: UIButton) {
-        searchTextField.endEditing(true)
-        print(searchTextField.text)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
-        return true
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text != "" {
-            return true
-        } else {
-            textField.placeholder = "Type something"
-            return false
-        }
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        if let city = searchTextField.text {
-            weatherManager.fetchWeather(cityName: city)
-        }
-        searchTextField.text = ""
-    }
-    
-    
 }
 
+//MARK: - UITextFieldDelegate
+
+
+    extension WeatherViewController: UITextFieldDelegate {
+        
+        @IBAction func searchPressed(_ sender: UIButton) {
+            searchTextField.endEditing(true)
+            print(searchTextField.text)
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchTextField.endEditing(true)
+            print(searchTextField.text!)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if textField.text != "" {
+                return true
+            } else {
+                textField.placeholder = "Type something"
+                return false
+            }
+        }
+
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            
+            if let city = searchTextField.text {
+                weatherManager.fetchWeather(cityName: city)
+            }
+            searchTextField.text = ""
+        }
+    }
+
+
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
+class <#name#>: <#super class#> {
+    <#code#>
+}
--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -8,50 +8,61 @@
 
 import Foundation
 
+protocol WeatherManagerDelegate {
+    func didUpdateteWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=b72eaedf7a501629cbf0ddb74f2f485e&units=metric"
     
+    var delegate: WeatherManagerDelegate?
+    
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        performRequest(with: urlString)
     }
-        func performRequest(urlString: String) {
-            if let url = URL(string: urlString) {
-                let session = URLSession(configuration: .default)
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            // Give the session a task
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    print(error!)
+                    return
+                }
                 
-                // Give the session a task
-                
-                let task = session.dataTask(with: url) { (data, response, error) in
-                    if error != nil {
-                        print(error!)
-                        return
-                    }
-                    
-                    if let safeData = data {
-                        let dataString = String(data: safeData, encoding: .utf8)
-                        self.parseJSON(weatherData: safeData)
-                      
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
                     }
                 }
-                    // Start the task
-                task.resume()
             }
-        }
-    
-    func parseJSON(weatherData: Data) {
-        let decoder = JSONDecoder()
-        do {
-         let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-         let id = decodedData.weather[0].id
-         let temp = decodedData.main.temp
-         let name = decodedData.name
-            
-            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
-            print(weather.temperatureString)
-        } catch {
-            print(error)
+            // Start the task
+            task.resume()
         }
     }
     
-
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+        } catch {
+            delegate?.didFailWithError(error: error)
+            print(error)
+            return nil
+        }
+    }
+    
+    
 }
