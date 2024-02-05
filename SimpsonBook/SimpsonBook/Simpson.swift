706a2ece199dc2556d9ca618369d03ef680e7950
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2024-02-05 05:06:26 +0300

    add detailsVC and Simpson model

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by MİZGİN YILDIRAK on 4.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+}
--- SimpsonBook/detailsVC.swift
+++ SimpsonBook/detailsVC.swift
@@ -8,15 +8,20 @@
 import UIKit
 
 class detailsVC: UIViewController {
-    @IBOutlet weak var nameLabel: UILabel!
-    
-    @IBOutlet weak var jobLabel: UILabel!
+
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
 
         // Do any additional setup after loading the view.
     }
-
 }
